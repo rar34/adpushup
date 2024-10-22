9c036e1bb3f547205e737cc0685a6d6d1434f750
rar34 <ra.cse34@gmail.com>
2024-10-22 15:52:17 +0600

    initialized project

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div>
+            this is homepage
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import HomePage from "../HomePage/HomePage";
+
+const Main = () => {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+};
+
+export default Main;
