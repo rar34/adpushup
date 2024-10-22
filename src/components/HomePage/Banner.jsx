ed8f0097d464c071e8104ed78888f9b99db945b3
rar34 <ra.cse34@gmail.com>
2024-10-22 20:46:31 +0600

    add slider into banner

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -1,6 +1,32 @@
 import { FaCheck } from "react-icons/fa";
 
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+// import "./styles.css";
+
+const images = [
+    "https://i.ibb.co.com/rpD2M0c/gsm.jpg",
+    "https://i.ibb.co.com/6bqgLJ5/ndtv.jpg",
+    "https://i.ibb.co.com/9WPdPgr/news.jpg"
+];
+
+
 const Banner = () => {
+
+    const [sliderRef] = useKeenSlider({
+        loop: true,
+        breakpoints: {
+            "(min-width: 400px)": {
+                slides: { perView: 2, spacing: 5 },
+            },
+            "(min-width: 1000px)": {
+                slides: { perView: 3, spacing: 10 },
+            },
+        },
+        slides: { perView: 1 },
+    });
+
+
     return (
         <div className="bg-primary">
             {/* logo */}
@@ -12,6 +38,14 @@ const Banner = () => {
                         <p className="flex items-center gap-2"><FaCheck className="text-2xl bg-green-600 text-white" /><span className="text-2xl text-white">Exclusive access to direct deals.</span></p>
                         <p className="flex items-center gap-2"><FaCheck className="text-2xl bg-green-600 text-white" /><span className="text-2xl text-white">Increased competition for your ad inventory.</span></p>
                         <p className="flex items-center gap-2"><FaCheck className="text-2xl bg-green-600 text-white" /><span className="text-2xl text-white">Ad fraud detection with Google policy compliance.</span></p>
+                    </div>
+                    {/* slider for banner */}
+                    <div className="py-10">
+                        <div ref={sliderRef} className="keen-slider max-w-2xl rounded-md bg-white p-5">
+                            <div className="keen-slider__slide number-slide1"><img src="https://i.ibb.co.com/rpD2M0c/gsm.jpg" alt="" /></div>
+                            <div className="keen-slider__slide number-slide2"><img src="https://i.ibb.co.com/6bqgLJ5/ndtv.jpg" alt="" /></div>
+                            <div className="keen-slider__slide number-slide3"><img src="https://i.ibb.co.com/9WPdPgr/news.jpg" alt="" /></div>
+                        </div>
                     </div>
                 </div>
             </div>
